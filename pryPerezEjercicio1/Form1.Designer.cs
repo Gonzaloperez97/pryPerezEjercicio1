7e43b02f9eb32dbf11baa2aa000a590a36605969
Gonzaloperez97 <gonzi.bernardo@gmail.com>
2025-10-20 17:42:18 -0300

    Finaliza el diseño del formulario de login.

--- pryPerezEjercicio1/Form1.Designer.cs
+++ pryPerezEjercicio1/Form1.Designer.cs
@@ -28,43 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            lblUsuario = new Label();
+            lblContraseña = new Label();
+            lblModulo = new Label();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            cmbModulo = new ComboBox();
             SuspendLayout();
             // 
-            // button1
+            // btnAceptar
             // 
-            button1.Location = new Point(266, 174);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnAceptar.Location = new Point(92, 259);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(75, 23);
+            btnAceptar.TabIndex = 0;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnCancelar
             // 
-            button2.Location = new Point(276, 259);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btnCancelar.Location = new Point(188, 259);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.TabIndex = 1;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(55, 102);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(47, 15);
+            lblUsuario.TabIndex = 2;
+            lblUsuario.Text = "Usuario";
+            // 
+            // lblContraseña
+            // 
+            lblContraseña.AutoSize = true;
+            lblContraseña.Location = new Point(53, 152);
+            lblContraseña.Name = "lblContraseña";
+            lblContraseña.Size = new Size(67, 15);
+            lblContraseña.TabIndex = 3;
+            lblContraseña.Text = "Contraseña";
+            // 
+            // lblModulo
+            // 
+            lblModulo.AutoSize = true;
+            lblModulo.Location = new Point(53, 200);
+            lblModulo.Name = "lblModulo";
+            lblModulo.Size = new Size(49, 15);
+            lblModulo.TabIndex = 4;
+            lblModulo.Text = "Módulo";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(170, 99);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(121, 23);
+            txtUsuario.TabIndex = 5;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(170, 149);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(121, 23);
+            txtContraseña.TabIndex = 6;
+            // 
+            // cmbModulo
+            // 
+            cmbModulo.FormattingEnabled = true;
+            cmbModulo.Location = new Point(170, 197);
+            cmbModulo.Name = "cmbModulo";
+            cmbModulo.Size = new Size(121, 23);
+            cmbModulo.TabIndex = 7;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(363, 450);
+            Controls.Add(cmbModulo);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtUsuario);
+            Controls.Add(lblModulo);
+            Controls.Add(lblContraseña);
+            Controls.Add(lblUsuario);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private Button btnAceptar;
+        private Button btnCancelar;
+        private Label lblUsuario;
+        private Label lblContraseña;
+        private Label lblModulo;
+        private TextBox txtUsuario;
+        private TextBox txtContraseña;
+        private ComboBox cmbModulo;
     }
 }
